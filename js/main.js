e4ccc3affff6766ef48feb21be1b487780decfb3
Samantha Bolotine <slb1992@yahoo.com>
2016-09-15 22:42:24 -0400

    menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).on('ready', function() {
 
-
+  $('h2').on('click', function() {
+    $(this).next('.menuSect').slideToggle(1000);
+    var $current = $(this).next('.menuSect');
+    $('.menuSect').not($current).slideUp(1000);
+  });
 
 
 
