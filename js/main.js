e8ed49f555f5a250f12a953b418552a90e15b762
Samantha Bolotine <slb1992@yahoo.com>
2016-09-18 14:41:08 -0400

    footer js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,21 @@ $(document).on('ready', function() {
 
   $('h2').on('click', function() {
     $(this).next('.menuSect').slideToggle(1000);
+
     var $current = $(this).next('.menuSect');
     $('.menuSect').not($current).slideUp(1000);
   });
+
+
+  $('#footer-extras h3').on('click', function() {
+    if ($('.line').css('display') == 'none') {
+      $(this).nextAll('#footer-extras ul').slideToggle(500);
+    };
+
+    var $current = $(this).nextAll('#footer-extras ul');
+    $('#footer-extras ul').not($current).slideUp(500);
+  });
+
 
 
 
