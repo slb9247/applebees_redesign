784cbe3333d3a4a398a84372d1c9d6c5857b4596
Samantha Bolotine <slb1992@yahoo.com>
2017-03-10 14:38:38 -0500

    menu info and footer animations

--- js/main.js
+++ js/main.js
@@ -11,34 +11,21 @@ $(document).on('ready', function() {
 /* footer extras toggle at < 370px */
   $('#footer-extras h3').on('click', function() {
     if ($('.line').css('display') == 'none') {
-      $(this).nextAll('#footer-extras ul').slideToggle(500);
+      $(this).nextAll('section ul').slideToggle(500);
     }
 
-    // var $current = $(this).nextAll('#footer-extras ul');
-    // $('#footer-extras ul').not($current).slideUp(500);
+    var $current = $(this).nextAll('section ul');
+    $('section ul').not($current).slideUp(500);
   });
 
 /* footer extras toggle fix for window resize */
-  checkSize();
-
-  $(window).resize(checkSize);
-
-  function checkSize() {
+  $(window).resize(function(){
     if ($('.line').css('display') == 'block') {
-      $('#footer-extras ul').css('display', 'block');
+      $('section ul').css('display', 'block');
     } else {
-      $('#footer-extras ul').css('display', 'none');
+      $('section ul').css('display', 'none');
     }
-  }
-
-
-    // $(window).resize(function(){
-    //   if (window.matchMedia("(min-width: 370px)").matches) {
-    //       $('#footer-extras ul').css('display','block')
-    //   } else {
-    //     $('#footer-extras ul').css('display', 'none');
-    //   }
-    // });
+  });
 
 
 
