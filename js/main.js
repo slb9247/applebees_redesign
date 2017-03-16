825248bc53e8b7c471d45bf962f21c721a8cec34
Samantha Bolotine <slb1992@yahoo.com>
2017-03-16 14:02:22 -0400

    menu mobile and responsiveness

--- js/main.js
+++ js/main.js
@@ -1,22 +1,56 @@
 $(document).on('ready', function() {
 
-/* menu page sections */
+/* mobile menu sections toggle */
   $('h2').on('click', function() {
     $(this).next('.menuSect').slideToggle(1000);
 
     var $current = $(this).next('.menuSect');
     $('.menuSect').not($current).slideUp(1000);
+
+    // $('#footer-extras h3').on('click', function() {
+    //   if ($('.line').css('display') == 'none') {
+    //     $(this).nextAll('section ul').slideToggle(500);
+    //
+    //     var $current = $(this).nextAll('section ul');
+    //     $('section ul').not($current).slideUp(500);
+    //   }
+    // });
+
+
+
+
+
+
+
+
+/* mobile menu sections grayscale class */
+    $('h2').removeClass('active');
+    $(this).addClass('active');
+
+    //ONLY SHOW COLOR WHEN SLIDETOGGLE IS ACTIVE
   });
+
+
+/* mobile menu sections toggle fix for window resize */
+  $(window).resize(function(){
+    if ($('h2:nth-of-type(2)').css('background-image') == 'none') {
+      $('.menuSect').css('display', 'block');
+    } else {
+      $('.menuSect').css('display', 'none');
+    }
+  });
+
 
 /* footer extras toggle at < 370px */
   $('#footer-extras h3').on('click', function() {
     if ($('.line').css('display') == 'none') {
       $(this).nextAll('section ul').slideToggle(500);
-    }
 
-    var $current = $(this).nextAll('section ul');
-    $('section ul').not($current).slideUp(500);
+      var $current = $(this).nextAll('section ul');
+      $('section ul').not($current).slideUp(500);
+    }
   });
+
 
 /* footer extras toggle fix for window resize */
   $(window).resize(function(){
