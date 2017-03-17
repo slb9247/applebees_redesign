5dc0b834e6feabc9d7c9c4bc55cc7e852dc8f6c3
Samantha Bolotine <slb1992@yahoo.com>
2017-03-17 15:31:13 -0400

    menu

--- js/main.js
+++ js/main.js
@@ -2,28 +2,14 @@ $(document).on('ready', function() {
 
 /* mobile menu sections toggle */
   $('h2').on('click', function() {
-    $(this).next('.menuSect').slideToggle(1000);
+    if ($('h2:nth-of-type(2)').css('background-image') != 'none') {
+      $(this).next('.menuSect').slideToggle(1000);
 
-    var $current = $(this).next('.menuSect');
-    $('.menuSect').not($current).slideUp(1000);
+      var $current = $(this).next('.menuSect');
+      $('.menuSect').not($current).slideUp(1000);
+    }
 
-    // $('#footer-extras h3').on('click', function() {
-    //   if ($('.line').css('display') == 'none') {
-    //     $(this).nextAll('section ul').slideToggle(500);
-    //
-    //     var $current = $(this).nextAll('section ul');
-    //     $('section ul').not($current).slideUp(500);
-    //   }
-    // });
-
-
-
-
-
-
-
-
-/* mobile menu sections grayscale class */
+    /* mobile menu sections grayscale class */
     $('h2').removeClass('active');
     $(this).addClass('active');
 
@@ -31,7 +17,7 @@ $(document).on('ready', function() {
   });
 
 
-/* mobile menu sections toggle fix for window resize */
+/* mobile menu sections closed slideToggle fix for window resize */
   $(window).resize(function(){
     if ($('h2:nth-of-type(2)').css('background-image') == 'none') {
       $('.menuSect').css('display', 'block');
@@ -41,7 +27,8 @@ $(document).on('ready', function() {
   });
 
 
-/* footer extras toggle at < 370px */
+
+/* footer extras toggle */
   $('#footer-extras h3').on('click', function() {
     if ($('.line').css('display') == 'none') {
       $(this).nextAll('section ul').slideToggle(500);
@@ -52,7 +39,7 @@ $(document).on('ready', function() {
   });
 
 
-/* footer extras toggle fix for window resize */
+/* footer extras closed slideToggle fix for window resize */
   $(window).resize(function(){
     if ($('.line').css('display') == 'block') {
       $('section ul').css('display', 'block');
